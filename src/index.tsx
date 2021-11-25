50a8448205da5222a9739dd5349cac30e8b9b9ff
Juun Pak <june.mintlatte@gmail.com>
2021-11-25 15:35:34 +0900

    🚚 Move to importing globalstyles

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import App from 'components/App';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
+import GlobalStyles from 'styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
+      <GlobalStyles />
       <App />
     </ThemeProvider>
   </React.StrictMode>,
