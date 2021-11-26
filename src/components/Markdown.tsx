d938d3335a141404af1c1db1c548c4f8192893ac
Juun Pak <june.mintlatte@gmail.com>
2021-11-26 11:25:26 +0900

    ✨ Makeup about section

---
+++ src/components/Markdown.tsx
@@ -0,0 +1,50 @@
+import ReactMarkdown from 'react-markdown';
+import styled from 'styled-components';
+
+const MarkdownContainer = styled.div`
+  width: 514px;
+  padding: 16px;
+  background-color: #ffffff;
+  border-radius: 3px;
+  margin: 0 auto;
+  box-shadow: 8px 8px ${props => props.theme.color.mintColor};
+`;
+
+const MarkdownText = styled.p`
+  font-size: 2.6rem;
+  font-family: ${props => props.theme.font.boldFont};
+  margin-bottom: 24px;
+
+  span {
+    display: block;
+    font-family: ${props => props.theme.font.nomalFont};
+    font-size: 1.4rem;
+    font-weight: 300;
+    letter-spacing: -0.1em;
+    text-decoration: underline;
+  }
+`;
+
+const markdown = `
+  ![HTML5](https://img.shields.io/badge/html5-%23E34F26.svg?style=for-the-badge&logo=html5&logoColor=white)
+  ![CSS3](https://img.shields.io/badge/css3-%231572B6.svg?style=for-the-badge&logo=css3&logoColor=white)
+  ![SASS](https://img.shields.io/badge/SASS-hotpink.svg?style=for-the-badge&logo=SASS&logoColor=white)
+  ![React](https://img.shields.io/badge/react-%2320232a.svg?style=for-the-badge&logo=react&logoColor=%2361DAFB)
+  ![Styled Components](https://img.shields.io/badge/styled--components-DB7093?style=for-the-badge&logo=styled-components&logoColor=white)
+  ![Git](https://img.shields.io/badge/git-%23F05033.svg?style=for-the-badge&logo=git&logoColor=white)
+  ![GitHub](https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=white)
+  ![Visual Studio Code](https://img.shields.io/badge/Visual%20Studio%20Code-0078d7.svg?style=for-the-badge&logo=visual-studio-code&logoColor=white)
+`;
+
+const Markdown = () => {
+  return (
+    <MarkdownContainer>
+      <MarkdownText>
+        기술스택 뱃지<span>더 많은 뱃지를 보유하고 싶습니다!</span>
+      </MarkdownText>
+      <ReactMarkdown>{markdown}</ReactMarkdown>
+    </MarkdownContainer>
+  );
+};
+
+export default Markdown;
--- src/routes/About.tsx
+++ src/routes/About.tsx
@@ -1,5 +1,141 @@
+import styled from 'styled-components';
+import yellAbout from 'images/about_yell.png';
+import Markdown from 'components/Markdown';
+
+const AboutContainer = styled.div`
+  width: 100vw;
+  height: calc(100vh - 70px);
+  padding: 64px 128px;
+  display: flex;
+  justify-content: center;
+  align-items: flex-start;
+  overflow: auto;
+
+  ::-webkit-scrollbar {
+    width: 8px;
+  }
+
+  ::-webkit-scrollbar-thumb {
+    border: 8px solid ${props => props.theme.color.yellowColor};
+    border-radius: 10px;
+    background: ${props => props.theme.color.yellowColor};
+    background-clip: padding-box;
+  }
+`;
+
+const ImgContainer = styled.div`
+  position: relative;
+  margin-right: 64px;
+`;
+
+interface IYellImg {
+  src: string;
+  alt: string;
+}
+
+const YellImg = styled.img<IYellImg>`
+  display: block;
+  width: 250px;
+  height: auto;
+  position: relative;
+  z-index: 2;
+`;
+
+const ImgBack = styled.div`
+  position: absolute;
+  top: 50px;
+  left: -29px;
+  display: block;
+  content: '';
+  width: 260px;
+  height: 260px;
+  border-radius: 50%;
+  background-color: ${props => props.theme.color.yellowColor};
+  z-index: 1;
+
+  ::after {
+    position: absolute;
+    top: -10px;
+    right: -50px;
+    display: block;
+    content: '';
+    width: 150px;
+    height: 150px;
+    border-radius: 50%;
+    background-color: ${props => props.theme.color.mintColor};
+  }
+`;
+
+const ContentsContainer = styled.div``;
+
+const AboutDescription = styled.div`
+  position: relative;
+  top: 16px;
+  background-color: #ffffff;
+  padding: 16px;
+  border-radius: 3px;
+  box-shadow: 8px 8px ${props => props.theme.color.yellowColor};
+  margin-bottom: 64px;
+`;
+
+const AboutTitle = styled.p`
+  font-family: ${props => props.theme.font.boldFont};
+  font-size: 3.2rem;
+  letter-spacing: -0.05em;
+  margin-bottom: 24px;
+
+  span {
+    font-size: 4rem;
+    color: ${props => props.theme.color.yellowColor};
+  }
+`;
+
+const AboutYell = styled.p`
+  width: 482px;
+  font-size: 1.8rem;
+  font-weight: 300;
+  letter-spacing: -0.1em;
+  text-align: justify;
+
+  strong {
+    font-weight: 500;
+    text-decoration: underline;
+  }
+
+  strong:first-child {
+    color: ${props => props.theme.color.mintColor};
+  }
+`;
+
 const About = () => {
-  return <h1>About</h1>;
+  return (
+    <AboutContainer>
+      <ImgContainer>
+        <YellImg src={yellAbout} alt="옐" />
+        <ImgBack />
+      </ImgContainer>
+      <ContentsContainer>
+        <AboutDescription>
+          <AboutTitle>
+            안녕하세요. <span>옐</span> 입니다!
+          </AboutTitle>
+          <AboutYell>
+            개발의 여러 분야 중에서도 <strong>프론트엔드를 택한 것</strong>은,
+            <br />
+            <strong>눈으로 보이는 결과물</strong>을 만드는 것이 좋았기
+            때문입니다. 그래서 UI로 색감을 다채롭게 쓰는 것을 좋아하며 UX로 더
+            나은 방향이 무엇일지 고민합니다.
+            <br />
+            <br />
+            <strong>어떤 일이든 협업을 바탕으로 이루어진다는 걸 인지</strong>
+            하고 있으며 그 밑바탕이 서로 간의 대화라고 생각합니다. 그렇기 때문에
+            일의 원활한 진행을 위해 팀원들과 지속적인 소통을 합니다.
+          </AboutYell>
+        </AboutDescription>
+        <Markdown />
+      </ContentsContainer>
+    </AboutContainer>
+  );
 };
 
 export default About;
