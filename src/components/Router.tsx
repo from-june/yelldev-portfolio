1bf1ff2f5d34ba7d65f77c1846cc8096c5b6e39c
Juun Pak <june.mintlatte@gmail.com>
2021-11-27 15:00:39 +0900

    ⚡️ Implement scroll to the top moving routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,5 +1,6 @@
 import { HashRouter, Routes, Route } from 'react-router-dom';
 import Header from 'components/Header';
+import ScrollTop from 'components/ScrollTop';
 import Home from 'routes/Home';
 import About from 'routes/About';
 import Contact from 'routes/Contact';
@@ -8,6 +9,7 @@ import Projects from 'routes/Projects';
 const Router = () => (
   <HashRouter>
     <Header />
+    <ScrollTop />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
